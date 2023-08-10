034162093c3afb88bbd3d49ddd89a66fca5fdc9f
lgalanruge <lgalanruge@gmail.com>
2023-08-10 10:57:08 -0500

    SetUp project

---
+++ src/main/java/co/com/myphotosnameapp/MyPhotosWebBackend/MyPhotosWebBackendApplication.java
@@ -0,0 +1,13 @@
+package co.com.myphotosnameapp.MyPhotosWebBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhotosWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhotosWebBackendApplication.class, args);
+	}
+
+}
